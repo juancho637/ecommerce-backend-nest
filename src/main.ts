d85f25b111fc3c3396a62d27dee3bee9c737429c
Juan David Garcia <juancho637@gmail.com>
2021-09-23 11:54:47 -0500

    integración y configuración del paquete para la generación automática de la documentación

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('Api for a simple e-commerce')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
