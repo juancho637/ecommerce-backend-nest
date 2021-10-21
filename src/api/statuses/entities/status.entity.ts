0bcbdec144d8b31939ad7bf9ac59c6f5cecf1de7
Juan David Garcia <juancho637@gmail.com>
2021-10-21 15:56:32 -0500

    integración de serialización para excluir columnas en las respuestas

--- src/api/statuses/entities/status.entity.ts
+++ src/api/statuses/entities/status.entity.ts
@@ -9,6 +9,7 @@ import {
 import { StatusNames } from '../enums/status-names.enum';
 import { StatusAbbreviations } from '../enums/status-abbreviations.enum';
 import { StatusTypes } from '../enums/status-types.enum';
+import { Exclude } from 'class-transformer';
 
 @Entity({ name: 'statuses' })
 export class Status {
@@ -18,18 +19,22 @@ export class Status {
   @Column({ nullable: false })
   name: StatusNames;
 
+  @Exclude()
   @Column()
   abbreviation: StatusAbbreviations;
 
+  @Exclude()
   @Column()
   type: StatusTypes;
 
   @Column()
   description: string;
 
+  @Exclude()
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
