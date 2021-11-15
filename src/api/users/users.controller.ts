0d95de11a3eb48b3dc11b9f65200046237d6cd67
Juan David Garcia <juancho637@gmail.com>
2021-11-15 09:12:35 -0500

    integración del serializer en el controlador

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -10,11 +10,13 @@ import {
   Query,
 } from '@nestjs/common';
 import { FormDataRequest } from 'nestjs-form-data';
+import { plainToClass } from 'class-transformer';
 
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FilterUsersDto } from './dto/filter-users.dto';
+import { UsersSerializer } from './users.serializer';
 
 @Controller('users')
 export class UsersController {
@@ -22,18 +24,24 @@ export class UsersController {
 
   @Post()
   @FormDataRequest()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    const userSaved = await this.usersService.create(createUserDto);
+
+    return plainToClass(UsersSerializer, userSaved);
   }
 
   @Get()
   findAll(@Query() filterUsersDto: FilterUsersDto) {
-    return this.usersService.findAll(filterUsersDto);
+    const users = this.usersService.findAll(filterUsersDto);
+
+    return plainToClass(UsersSerializer, users);
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+    const user = this.usersService.findOne(id);
+
+    return plainToClass(UsersSerializer, user);
   }
 
   @Patch(':id')
@@ -42,11 +50,15 @@ export class UsersController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(id, updateUserDto);
+    const userUpdated = this.usersService.update(id, updateUserDto);
+
+    return plainToClass(UsersSerializer, userUpdated);
   }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.remove(id);
+    const userDeleted = this.usersService.remove(id);
+
+    return plainToClass(UsersSerializer, userDeleted);
   }
 }
