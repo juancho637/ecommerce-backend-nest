21805009566d6359fa58e6ba58b624352d35edcb
Juan David Garcia <juancho637@gmail.com>
2021-11-15 09:07:38 -0500

    modificación del nombre de la funcionalidad para buscar a un usuario por email

--- src/api/users/users.repository.ts
+++ src/api/users/users.repository.ts
@@ -1,9 +1,10 @@
 import { EntityRepository, Repository } from 'typeorm';
+
 import { User } from './entities/user.entity';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  public async findByEmail(email: string): Promise<User> {
+  public async findOneByEmail(email: string): Promise<User> {
     return await this.findOne({ where: { email } });
   }
 }
