a9c23449665ab333ec2571e7e8c7235dcbfeae80
Juan David Garcia <juancho637@gmail.com>
2021-11-15 09:06:29 -0500

    integración de funcionalidad para realizar la búsqueda de los estados por la abreviación

--- src/api/statuses/statuses.repository.ts
+++ src/api/statuses/statuses.repository.ts
@@ -1,5 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
+
 import { Status } from './entities/status.entity';
+import { StatusAbbreviations } from './enums/status-abbreviations.enum';
 
 @EntityRepository(Status)
-export class StatusesRepository extends Repository<Status> {}
+export class StatusesRepository extends Repository<Status> {
+  public async findOneByAbbreviation(
+    abbreviation: StatusAbbreviations,
+  ): Promise<Status> {
+    return await this.findOne({ where: { abbreviation } });
+  }
+}
