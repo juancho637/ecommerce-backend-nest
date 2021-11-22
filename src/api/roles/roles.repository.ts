c0988355e684a9c17b95b91ddb51a1a942a7dfef
Juan David Garcia <juancho637@gmail.com>
2021-11-22 13:34:32 -0500

    integración de funcionalidad para buscar roles por el nombre

--- src/api/roles/roles.repository.ts
+++ src/api/roles/roles.repository.ts
@@ -1,6 +1,11 @@
 import { EntityRepository, Repository } from 'typeorm';
 
 import { Role } from './entities/role.entity';
+import { RoleNames } from './enums/role-names.enum';
 
 @EntityRepository(Role)
-export class RolesRepository extends Repository<Role> {}
+export class RolesRepository extends Repository<Role> {
+  public async findOneByName(name: RoleNames): Promise<Role> {
+    return await this.findOne({ where: { name } });
+  }
+}
